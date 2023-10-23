6c5cddb8dd6e45a03301642c104818f87f3655c1
weiluo <luoweiforever@gmail.com>
2023-10-22 22:55:27 -0400

    checkpoint 20230928 -- add patch v1

--- nn_models/tf/image/swin/__init__.py
+++ nn_models/tf/image/swin/__init__.py
@@ -1,1 +1,9 @@
 # https://github.com/rishigami/Swin-Transformer-TF/blob/main/swintransformer/model.py
+# Swin https://www.youtube.com/watch?v=luP3-Fs0QCo&t=2944s&ab_channel=MuLi
+
+# paper
+# 1. https://paperswithcode.com/paper/image-as-a-foreign-language-beit-pretraining
+# 2. https://paperswithcode.com/paper/vision-transformer-adapter-for-dense
+# 3 https://paperswithcode.com/paper/mask-dino-towards-a-unified-transformer-based-1
+# 4 https://paperswithcode.com/paper/semask-semantically-masked-transformers-for-1
+# 5 https://paperswithcode.com/paper/masked-attention-mask-transformer-for
